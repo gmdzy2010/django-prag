5121e620d919272cb83ecf390f42fb268942d8a9
yang_ge <gmdzy2010@users.noreply.github.com>
2018-07-18 22:05:37 +0800

    Update get_original_data.py

--- core/get_original_data.py
+++ core/get_original_data.py
@@ -7,25 +7,6 @@ import re
 # from .exceptions import *
 
 
-DATA_SOURCE = {
-    "db": {
-        "host": "192.168.0.100",
-        "port": 5432,
-        "database": "colowell",
-        "user": "postgres",
-        "password": "postgres",
-    },
-    "file": {
-        "path": "/usr/bin/",
-    },
-    "stdout": {
-        "info": "This function hadn't been developed, please wait for updating"
-                "in next version",
-        "command": "ps aux|grep test",
-    },
-}
-
-
 class MethodIllegalException(Exception):
     """Method illegal exception"""
     pass
