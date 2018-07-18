41e0c31a352bea0c1a86e87f0d0e6eca64ace6b8
yang_ge <gmdzy2010@users.noreply.github.com>
2018-07-18 21:28:19 +0800

    Update settings.py

--- conf/settings.py
+++ conf/settings.py
@@ -1,1 +1,17 @@
-
+DATA_SOURCE = {
+    "db": {
+        "host": "192.168.0.100",
+        "port": 5432,
+        "database": "colowell",
+        "user": "postgres",
+        "password": "postgres",
+    },
+    "file": {
+        "path": "/usr/bin/",
+    },
+    "stdout": {
+        "info": "This function hadn't been developed, please wait for updating"
+                "in next version",
+        "command": "ps aux|grep test",
+    },
+}
