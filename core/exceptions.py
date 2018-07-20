a5c86036af59bc3d73e8bcf11893ab5f07d4ea98
yang_ge <gmdzy2010@users.noreply.github.com>
2018-07-20 09:48:19 +0800

    Update exceptions.py

--- core/exceptions.py
+++ core/exceptions.py
@@ -1,5 +1,15 @@
+"""
+This file contain all of the exceptions occurring during rendering the html
+templates to pdf file
+"""
+
 class MethodIllegalException(Exception):
-    """
-    Method illegal exception
-    """
+    """Method illegal exception"""
+    pass
+
+
+class ContextEmptyException(Exception):
+    """The context data rendered into the template must not be empty, or will
+     raise this exception.
+     """
     pass
