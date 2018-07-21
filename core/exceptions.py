3e2cf514008134bcb2cbe73b7dac1f8eb7fcf63a
yang_ge <gmdzy2010@users.noreply.github.com>
2018-07-21 23:45:17 +0800

    Update exceptions.py

--- core/exceptions.py
+++ core/exceptions.py
@@ -13,3 +13,17 @@ class ContextEmptyException(Exception):
      raise this exception.
      """
     pass
+
+
+class PathIllegalException(Exception):
+    """The context data rendered into the template must not be empty, or will
+    raise this exception.
+    """
+    pass
+
+
+class FileAccessException(Exception):
+    """The file wanted didn't own permission of read, write or execute would
+    raise this exception.
+    """
+    pass
