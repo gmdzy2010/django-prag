b74dc99f8263d9c99a8eaf198595d95e87849314
yang_ge <gmdzy2010@users.noreply.github.com>
2018-07-21 23:38:07 +0800

    Update configurations.py

--- conf/configurations.py
+++ conf/configurations.py
@@ -1,16 +1,46 @@
+"""
+This file contains all of the settings for this package.
+"""
+
+import os
+
+
+# Package name
+
+PACKAGE_NAME = "realbio_prag"
+
+PACKAGE_ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Data source initial settings
+
 DATA_SOURCE = {
     "db": {
-        "host": "",
-        "port": ,
-        "database": "",
-        "user": "",
-        "password": "",
+        "host": "192.168.0.100",
+        "port": 5432,
+        "database": "colowell",
+        "user": "postgres",
+        "password": "postgres",
     },
     "file": {
-        "path": "",
+        "path": "/usr/bin/",
     },
     "stdout": {
-        "info": "",
-        "command": "",
+        "info": "This function hadn't been developed, please wait for updating"
+                "in next version",
+        "command": "ps aux|grep test",
+    },
+    "django": {
+        "info": "This function hadn't been developed, please wait for updating"
+                "in next version",
     },
 }
+
+
+# Path settings
+
+LOGGING_FILE_PATH = os.path.join(PACKAGE_ROOT_PATH, "logs")
+
+TEMPLATE_PATH = os.path.join(PACKAGE_ROOT_PATH, "templates")
+
+TEMPLATE_PATH_RENDERED = os.path.join(PACKAGE_ROOT_PATH, "rendered_file")
